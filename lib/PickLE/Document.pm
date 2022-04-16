9e14f661394977ef22bf1926e31870954417bdfc
Nathan Campos <nathan@innoveworkshop.com>
2022-04-16 17:11:57 +0100

    Added categories and packages to PickLE::Document and implemented foreach methods for them

--- lib/PickLE/Document.pm
+++ lib/PickLE/Document.pm
@@ -13,6 +13,8 @@ use warnings;
 use autodie;
 use Moo;
 use Carp;
+use List::Util qw(any);
+use Scalar::Util qw(reftype);
 
 use PickLE::Component;
 use PickLE::Parser;
@@ -28,9 +30,18 @@ use PickLE::Parser;
 
   # Load from file.
   $doc = PickLE::Document->load("example.pkl");
-  foreach my $component (@{$doc->components}) {
-      say $component->name;
-  }
+
+  # List all components.
+  $doc->foreach_component(sub {
+  	my ($component) = @_;
+  	say $component->name;
+  });
+
+  # Filter out some components.
+  $doc->foreach_component({ category => 'Resistors' }, sub {
+  	my ($component) = @_;
+  	say $component->name;
+  });
 
 =head1 ATTRIBUTES
 
@@ -47,6 +58,32 @@ has components => (
 	lazy    => 1,
 	default => sub { [] },
 	writer  => '_set_components'
+);
+
+=item I<categories>
+
+List of all of the categories available in the document.
+
+=cut
+
+has categories => (
+	is      => 'ro',
+	lazy    => 1,
+	default => sub { [] },
+	writer  => '_set_categories'
+);
+
+=item I<packages>
+
+List of all of the component packages available in the document.
+
+=cut
+
+has packages => (
+	is      => 'ro',
+	lazy    => 1,
+	default => sub { [] },
+	writer  => '_set_packages'
 );
 
 =back
@@ -87,7 +124,94 @@ document.
 
 sub add_component {
 	my $self = shift;
-	push @{$self->components}, @_;
+
+	# Go through components.
+	foreach my $component (@_) {
+		# Add the component to the components list.
+		push @{$self->components}, $component;
+
+		# Make sure to update the categories and packages list.
+		$self->_auto_add_category($component->category);
+		$self->_auto_add_package($component->case);
+	}
+}
+
+=item I<$doc>->C<foreach_component>([I<\%filter>,] I<$coderef>)
+
+Executes a block of code (I<$coderef>) for each component. Where the component
+object is passed as the first argument.
+
+There's an optional I<\%filter> hash reference that can be passed with
+attributes I<category> and/or I<case> in order to only iterate through
+components match the criteria.
+
+=cut
+
+sub foreach_component {
+	my $self = shift;
+	my $filter = undef;
+	my $coderef;
+
+	# Check if we actually have a filter.
+	if (reftype($_[0]) eq 'HASH') {
+		$filter = shift;
+	}
+	$coderef = shift;
+
+	# Go through the components.
+	foreach my $component (@{$self->components}) {
+		# Do we have filters?
+		if (defined $filter) {
+			# Category filter.
+			if (exists $filter->{category}) {
+				if ($component->category ne $filter->{category}) {
+					next;
+				}
+			}
+
+			# Package filter.
+			if (exists $filter->{case}) {
+				if ($component->case ne $filter->{case}) {
+					next;
+				}
+			}
+		}
+
+		# Call the coderef given by the caller.
+		$coderef->($component);
+	}
+}
+
+=item I<$doc>->C<foreach_category>(I<$coderef>)
+
+Executes a block of code (I<$coderef>) for each category available in the
+document. Where the category name is passed as the first argument.
+
+=cut
+
+sub foreach_category {
+	my ($self, $coderef) = @_;
+
+	# Go through the categories.
+	foreach my $category (@{$self->categories}) {
+		$coderef->($category);
+	}
+}
+
+=item I<$doc>->C<foreach_package>(I<$coderef>)
+
+Executes a block of code (I<$coderef>) for each package available in the
+document. Where the package name is passed as the first argument.
+
+=cut
+
+sub foreach_package {
+	my ($self, $coderef) = @_;
+
+	# Go through the package.
+	foreach my $case (@{$self->packages}) {
+		$coderef->($case);
+	}
 }
 
 =item I<$doc>->C<save>(I<$filename>)
@@ -131,17 +255,61 @@ sub as_string {
 
 =over 4
 
-=item I<$self>->C<_set_components>(I<$components>)
+=item I<$self>->C<_auto_add_category>(I<$category>)
 
-Sets the I<components> attribute internally.
+Adds a I<category> to the I<categories> attribute in case it isn't in the list
+yet.
+
+=cut
+
+sub _auto_add_category {
+	my ($self, $category) = @_;
+
+	# Check if we actually got something useful.
+	if (not defined $category) {
+		return;
+	}
+
+	# Check if it's already in the categories list.
+	if (any { $_ eq $category } @{$self->categories}) {
+		return;
+	}
+
+	# Add the new one to the list.
+	push @{$self->categories}, $category;
+}
+
+=item I<$self>->C<_auto_add_package>(I<$case>)
+
+Adds a package (I<case>) to the I<packages> attribute in case it isn't in the
+list yet.
+
+=cut
+
+sub _auto_add_package {
+	my ($self, $case) = @_;
+
+	# Check if we actually got something useful.
+	if (not defined $case) {
+		return;
+	}
+
+	# Check if it's already in the packages list.
+	if (any { $_ eq $case } @{$self->packages}) {
+		return;
+	}
+
+	# Add the new one to the list.
+	push @{$self->packages}, $case;
+}
+
+=back
 
 =cut
 
 1;
 
 __END__
-
-=back
 
 =head1 AUTHOR
 
